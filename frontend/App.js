9a5c5fb970f52435f58b09df5feafa3e45b69f8d
zach <zclewell@gmail.com>
2019-02-09 15:55:14 -0600

    Loading content from utl

--- frontend/App.js
+++ frontend/App.js
@@ -1,11 +1,52 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, ActivityIndicator, Text, View, StyleSheet } from 'react-native';
 
-export default class App extends React.Component {
+export default class FetchExample extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { isLoading: true }
+  }
+
+  componentDidMount() {
+    return fetch('https://facebook.github.io/react-native/movies.json')
+      .then((response) => {
+        console.log(response)
+        return response.json()
+      })
+      .then((responseJson) => {
+        this.setState({
+          isLoading: false,
+          dataSource: responseJson.movies,
+        }, function () {
+
+        });
+
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+
+
   render() {
+
+    if (this.state.isLoading) {
+      return (
+        <View style={styles.container}>
+          <ActivityIndicator />
+        </View>
+      )
+    }
+
     return (
       <View style={styles.container}>
-        <Text>Hello Peilun!</Text>
+        <FlatList
+          data={this.state.dataSource}
+          renderItem={({ item }) => <Text>{item.title}, {item.releaseYear}</Text>}
+          keyExtractor={({ id }, index) => id}
+        />
       </View>
     );
   }
@@ -17,5 +58,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 100
   },
 });
